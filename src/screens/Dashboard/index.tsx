3062677cbd2f5d2e2e1be5954b99f0fc083125c0
weescoelho <wes.coelho18@gmail.com>
2022-01-07 01:24:22 -0300

    Implementação do TransactionCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,28 +1,110 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardData } from '../../components/TransactionCard';
 
-import { Container, Header,UserWrapper,UserInfo, Photo , User, UserGreeting, UserName, Icon, HighlightCards} from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionsList,
+} from './styles';
+
+export interface DataListProps extends TransactionCardData {
+  id: string;
+}
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id:'1',
+      type: 'positive',
+      title: 'Desenvolvimento de app',
+      amount: 'R$ 10,00',
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign',
+      },
+      date: '10/10/2020',
+    },
+    {
+      id:'2',
+      type: 'negative',
+      title: 'Ifood',
+      amount: 'R$ 10,00',
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee',
+      },
+      date: '05/10/2020',
+    },
+    {
+      id:'3',
+      type: 'negative',
+      title: 'Aluguel do apartamento',
+      amount: 'R$ 10,00',
+      category: {
+        name: 'Casa',
+        icon: 'shopping-bag',
+      },
+      date: '11/10/2020',
+    },
+  ];
+
   return (
     <Container>
       <Header>
         <UserWrapper>
-        <UserInfo>
-          <Photo source={{uri: 'https://avatars.githubusercontent.com/u/68740380?v=4'}}/>
-          <User>
-            <UserGreeting>Olá,</UserGreeting>
-            <UserName>Weslley</UserName>
-          </User>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/68740380?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Weslley</UserName>
+            </User>
           </UserInfo>
-          <Icon name="power"/>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
       <HighlightCards>
-        <HighlightCard type='up' title='Entradas' amount='R$ 10,00' lastTransaction='última transação ontem'/>
-        <HighlightCard type='down' title='Saídas' amount='R$ 10,00' lastTransaction='última transação ontem'/>
-        <HighlightCard type='total' title='Total' amount='R$ 10,00' lastTransaction='última transação ontem'/>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 10,00"
+          lastTransaction="última transação ontem"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 10,00"
+          lastTransaction="última transação ontem"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 10,00"
+          lastTransaction="última transação ontem"
+        />
       </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionsList
+          data={data}
+          keyExtractor={(item: DataListProps) => item.id }
+          renderItem={({ item }) => <TransactionCard data={item as DataListProps} />}
+        />
+      </Transactions>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 import { Feather } from '@expo/vector-icons';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList, FlatListProps } from 'react-native';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.'
 
 export const Container = styled.View`
   background-color:${({ theme }) => theme.colors.background};
@@ -74,3 +77,21 @@ export const HighlightCards = styled.ScrollView.attrs({
   margin-top:${RFPercentage(20)}px;
 
 `
+
+export const Transactions = styled.View`
+  flex:1;
+  padding:0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`
+
+export const Title = styled.Text`
+  font-size:${RFValue(18)}px;
+   font-family:${({ theme }) => theme.fonts.regular};
+   margin-bottom:16px;
+`
+
+export const TransactionsList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: getBottomSpace() }
+})``
